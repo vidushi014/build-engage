c9df0f5eb70d213a6ec3f721ac937ab0ca0789d1
Vidushi Singh <76548398+vidushi014@users.noreply.github.com>
2022-05-29 23:36:14 +0530

    we are in endgame now

---
+++ precache-manifest.047388110a8afd9ee0c617c7f4f4ef47.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "9c57cd0a4f5a2dff2122",
+    "url": "/static/css/main.a36ffe8a.chunk.css"
+  },
+  {
+    "revision": "9c57cd0a4f5a2dff2122",
+    "url": "/static/js/main.9c57cd0a.chunk.js"
+  },
+  {
+    "revision": "e8100a7e04f515f37629",
+    "url": "/static/js/1.e8100a7e.chunk.js"
+  },
+  {
+    "revision": "ade9f02e52c2cb2164f1",
+    "url": "/static/js/2.ade9f02e.chunk.js"
+  },
+  {
+    "revision": "69b11a1d0ee090c85547",
+    "url": "/static/js/runtime~main.69b11a1d.js"
+  },
+  {
+    "revision": "2f3b5f2e779ed84820ab4e219d4c3fab",
+    "url": "/static/media/home.2f3b5f2e.svg"
+  },
+  {
+    "revision": "f98af078d171228115db5e3508bec523",
+    "url": "/static/media/admin.f98af078.svg"
+  },
+  {
+    "revision": "7eca5b70c56318e2e84ae20450b4e3a3",
+    "url": "/static/media/refresh.7eca5b70.svg"
+  },
+  {
+    "revision": "8eb252d92935363814b39c00ef589519",
+    "url": "/static/media/record.8eb252d9.svg"
+  },
+  {
+    "revision": "b84e06b9f4a69369ff9a80cbb1b4aea4",
+    "url": "/static/media/dummy.b84e06b9.png"
+  },
+  {
+    "revision": "427c9258a379aad8a5d5cb16b705d2b7",
+    "url": "/static/media/blogHero.427c9258.jpg"
+  },
+  {
+    "revision": "888a916e4057826ff0e3ce2abd09562f",
+    "url": "/static/media/Advertisement.888a916e.png"
+  },
+  {
+    "revision": "206e5246826c3716fd94ea4827cd6d81",
+    "url": "/static/media/dummyAd1.206e5246.png"
+  },
+  {
+    "revision": "6d3826550c97e6ce8d6c186e071f9762",
+    "url": "/static/media/dummyAd2.6d382655.png"
+  },
+  {
+    "revision": "e70850bfe43029b24a26b9eaea96c974",
+    "url": "/static/media/mockup.e70850bf.png"
+  },
+  {
+    "revision": "fea1ff694cfde96457d84286e67e3638",
+    "url": "/index.html"
+  }
+];
